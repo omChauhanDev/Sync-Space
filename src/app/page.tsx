53d23706c861ac3c01a4849281c4f4cd7ffaa06f
Om Chauhan <omchauhan@Oms-MacBook-Pro.local>
2024-12-05 22:23:19 +0530

    added responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
       <NavBar />
       <div className='min-h-screen h-full flex items-center justify-center'>
         <BgPattern />
-        <section className='grid h-full px-20 flex-1 grid-cols-8 w-[90%] items-center justify-center gap-8 mx-auto'>
-          <div className='pl-16 h-full flex flex-col items-center justify-center col-span-3 gap-12'>
+        <section className='grid h-full md:px-20 px-7 flex-1 md:grid-cols-8 w-[90%] items-center justify-center gap-8 mx-auto grid-cols-1'>
+          <div className='md:pl-16 h-full flex flex-col items-center justify-center col-span-3 gap-12'>
             <h1 className='text-4xl bg-background'>
               Connect with Anyone, Anywhere
             </h1>
@@ -23,7 +23,7 @@ export default function Home() {
               <JoinSpace />
             </div>
           </div>
-          <div className='h-full col-span-5'>
+          <div className='h-full col-span-5 md:block hidden'>
             <Image
               alt='people in a video conference'
               width={1000}
