c9853c3c2cf57f6c5eba6c0bbf09d3ed47d6a83d
Om Chauhan <omchauhan@Oms-MacBook-Pro.local>
2024-12-12 23:08:08 +0530

    testing ec2 backend

--- src/context/socketProvider.tsx
+++ src/context/socketProvider.tsx
@@ -16,10 +16,19 @@ interface SocketProviderProps {
 }
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
-  const socket = useMemo(() => io("https://18.61.3.171:8000/mediasoup", {
-    secure: true,
-    rejectUnauthorized: false // WARNING: This disables SSL verification
-  }), []);
+  // const socket = useMemo(() => io("https://18.61.3.171:8000/mediasoup", {
+  const socket = useMemo(
+    () =>
+      io(
+        "https://18.61.3.171:8000/mediasoup"
+        // ,
+        // {
+        //   secure: true,
+        //   rejectUnauthorized: false, // WARNING: This disables SSL verification
+        // }
+      ),
+    []
+  );
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
